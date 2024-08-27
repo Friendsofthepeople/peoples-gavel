071c5b156142df705c4def29ceb5759780724f46
Collins O. Odhiambo <comon928@gmail.com>
2024-08-27 16:27:35 +0000

    add icon

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,22 @@
+<svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M17 33C25.8366 33 33 25.8366 33 17C33 8.16344 25.8366 1 17 1C8.16344 1 1 8.16344 1 17C1 25.8366 8.16344 33 17 33Z" stroke="url(#paint0_linear_615_1671)" stroke-width="1.5" stroke-miterlimit="6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M17.0001 32.9999C22.3615 32.9999 26.7078 27.2851 26.7078 20.2355C26.7078 13.186 22.3615 7.47119 17.0001 7.47119C11.6387 7.47119 7.29248 13.186 7.29248 20.2355C7.29248 27.2851 11.6387 32.9999 17.0001 32.9999Z" stroke="url(#paint1_linear_615_1671)" stroke-width="1.5" stroke-miterlimit="6" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M16.9999 32.9999C19.2543 32.9999 21.0819 28.6841 21.0819 23.3603C21.0819 18.0365 19.2543 13.7207 16.9999 13.7207C14.7455 13.7207 12.918 18.0365 12.918 23.3603C12.918 28.6841 14.7455 32.9999 16.9999 32.9999Z" stroke="url(#paint2_linear_615_1671)" stroke-width="1.5" stroke-miterlimit="6" stroke-linecap="round" stroke-linejoin="round"/>
+<defs>
+<linearGradient id="paint0_linear_615_1671" x1="17" y1="1" x2="17" y2="33" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="0.495" stop-color="#BC0000"/>
+<stop offset="1" stop-color="#01C909"/>
+</linearGradient>
+<linearGradient id="paint1_linear_615_1671" x1="17.0001" y1="7.47119" x2="17.0001" y2="32.9999" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="0.495" stop-color="#BC0000"/>
+<stop offset="1" stop-color="#01C909"/>
+</linearGradient>
+<linearGradient id="paint2_linear_615_1671" x1="16.9999" y1="13.7207" x2="16.9999" y2="32.9999" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="0.495" stop-color="#BC0000"/>
+<stop offset="1" stop-color="#01C909"/>
+</linearGradient>
+</defs>
+</svg>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
      <h2>Welcome to Bonga</h2>
     </main>
   );
