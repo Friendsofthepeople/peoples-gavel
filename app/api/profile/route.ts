a9053c81353962269d78e32cfa9a02fc865c90af
comon928 <comon928@gmail.com>
2024-09-04 06:33:11 +0000

    remove token

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -1,13 +1,8 @@
-import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI1NDQxMzE2LCJpYXQiOjE3MjUzNTQ5MTYsImp0aSI6IjVjNmQwOWJlZTU0MDRkYTU5MmE4MWE3ODlkNThlMDdjIiwidXNlcl9pZCI6MX0.mGLUpFkkL342Z9R95dyX-1KZkLQbnYvmC68EPTgSkOU"
-// const token = process.env.NEXT_PUBLIC_TOKEN
-// const user_id = process.env.NEXT_PUBLIC_USER_ID
-// const page_id = process.env.NEXT_PUBLIC_PAGE_ID
+const token = ""
 
 const endpoint = `http://www.recallserver.co:8000/auth/users/me`;
-
 
 export async function GET() {
   try{
